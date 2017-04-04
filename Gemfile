88df2755311ca30e4ed318657aa3d8c993502b35
kittysquee <kittysquee@gmail.com>
2017-04-04 12:21:42 +0100

    Adds first test

--- Gemfile
+++ Gemfile
@@ -4,8 +4,8 @@ ruby '2.4.0'
 
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
-gem 'rspec'
-gem 'capybara'
+gem 'rspec', group: :test
+gem 'capybara', group: :test
 gem 'sinatra'
 gem 'selenium-webdriver'
 # gem 'simplecov', require: false, group: :test
--- spec/features/see_links_spec.rb
+++ spec/features/see_links_spec.rb
@@ -1,6 +1,10 @@
 feature 'see links' do
   scenario 'user can see a list of links on homepage' do
-    visit ('/')
-    expect(page).to have_content 'Link'
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    visit '/links'
+    expect(page.status_code).to eq 200
+    within 'ul#links' do
+      expect(page).to have_content('Makers Academy')
+    end
   end
 end
