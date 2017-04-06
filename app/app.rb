17e8c6538d86bfc31aba2d0749375f72183c2522
Magnus Holm <mail@magnusholm.com>
2017-04-06 11:18:01 +0100

    add multiple tags

--- app/app.rb
+++ app/app.rb
@@ -18,8 +18,8 @@ class BookmarkManager < Sinatra::Base
 
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags])
-    link.tags << tag
+    tag = params[:tags].split
+    tag.each { |tag| link.tags << Tag.first_or_create(name: tag) }
     link.save
     redirect to('/links')
   end
--- spec/features/tag_spec.rb
+++ spec/features/tag_spec.rb
@@ -3,11 +3,21 @@ feature 'tag field' do
     visit '/links/new'
     fill_in 'url', with: 'google.co.uk'
     fill_in 'title', with: 'google'
-    fill_in 'tags', with: 'search engine'
+    fill_in 'tags', with: 'search-engine'
 
     click_button 'Add link'
     link = Link.first
-    expect(link.tags.map(&:name)).to include('search engine')
+    expect(link.tags.map(&:name)).to include('search-engine')
+    end
+
+    scenario 'can add multiple tags to links in bookmark-manager' do
+      visit '/links/new'
+      fill_in 'url', with: 'http://theguardian.com'
+      fill_in 'title', with: 'Newspaper'
+      fill_in 'tags', with: 'news entertainment sport politics travel'
+      click_button 'Add link'
+      link = Link.first
+      expect(link.tags.map(&:name)).to include('news', 'sport')
     end
 
 end
