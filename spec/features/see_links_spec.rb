778ded9d517cb2af64f3a2a32b9042c68e0f95e4
alicespyglass <alicespyglass@gmail.com>
2017-04-06 10:38:06 +0100

    16 - tags can be filtered

--- spec/features/see_links_spec.rb
+++ spec/features/see_links_spec.rb
@@ -4,7 +4,7 @@ feature 'see links' do
     Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy', tags: [Tag.first_or_create(name: 'education')])
     Link.create(url: 'http://www.scrubs.com', title: 'Scrubs', tags: [Tag.first_or_create(name: 'TV')])
     Link.create(url: 'http://www.mozart.com', title: 'Mozart', tags: [Tag.first_or_create(name: 'Culture')])
-    Link.create(url: 'http://www.paris.com', title: 'Paris', tags: [Tag.first_or_create(name: 'Travel and Fun')])
+    Link.create(url: 'http://www.paris.com', title: 'Paris', tags: [Tag.first_or_create(name: 'Travel')])
   end
 
   scenario 'user can see a list of links on homepage' do
@@ -17,6 +17,7 @@ feature 'see links' do
 
   scenario 'I can filter links by tag' do
     visit '/tags/TV'
+
     expect(page.status_code).to eq(200)
     within 'ul#links' do
       expect(page).not_to have_content('Makers Academy')
